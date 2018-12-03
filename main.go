9a6f401e28932994854c82bfa23ddf2e52f3ad39
Nader Eloshaiker <nader.eloshaiker@gmail.com>
2018-12-03 14:05:27 +1100

    Section 4 Lecture 40 - embedded structs

--- main.go
+++ main.go
@@ -2,18 +2,32 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName string
+	contact contactInfo
 }
 
 func main() {
 	//alex := person{"Alex", "Anderson"}
 	//alex := person{firstName: "Alex", lastName: "Anderson"}
-	var alex person
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
+	//var alex person
+	//alex.firstName = "Alex"
+	//alex.lastName = "Anderson"
 
-	fmt.Println(alex)
+	alex := person{
+		firstName: "Alex",
+		lastName: "Anderson",
+		contact: contactInfo{
+			email: "aa@email.com",
+			zipCode: 123,
+		},
+	}
+
 	fmt.Printf("%+v", alex)
 }
